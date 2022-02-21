0044074189090ee1c0dee1a694f6da2e6b91009e
vantyaevserega <33132682+vantyaevserega@users.noreply.github.com>
2022-02-21 15:23:07 +0500

    Разобран пакет даты и времени(вроде), переделал кнопки, добавил конфигурационный режим
    полезнвц линк https://github.com/prototux/PSA-RE/tree/master/buses/AEE2004.full/LS.CONF

--- resistButtons.ino
+++ resistButtons.ino
@@ -10,6 +10,8 @@ const int CS_XC = 2;
 const int INC_XC = 3;
 const int UD_XC = 4;
 
+int confPressedTime;
+bool isConfig = false;
 const int REVERSEPIN = 8;
 const int ILLUMINATEPIN = 9;
 int vv = 0;
@@ -24,9 +26,9 @@ int prevResist = 0;
 void setup() 
 {
   Serial.begin(115200);
-  Serial.println("Start INIT");
+  Serial.println("Start");
   mcp2515.reset();
-  mcp2515.setConfigMode();
+  //mcp2515.setConfigMode();
   /*
   mcp2515.setFilter(MCP2515::RXF0, false, 0x21F);
   mcp2515.setFilter(MCP2515::RXF1, false, 0x0A2);
@@ -34,10 +36,7 @@ void setup()
   mcp2515.setFilter(MCP2515::RXF3, false, 0x276);
   */  
   mcp2515.setBitrate(CAN_125KBPS, MCP_8MHZ);  
-  while (mcp2515.setNormalMode() != MCP2515::ERROR_OK) 
-  {
-    delay(100);
-  }
+  mcp2515.setNormalMode();
 
   // инициализация резистивных выводов  
   pinMode(CS_XC, OUTPUT);
@@ -51,126 +50,143 @@ void setup()
   pinMode(ILLUMINATEPIN, OUTPUT);
   digitalWrite(REVERSEPIN, LOW); 
   digitalWrite(ILLUMINATEPIN, LOW); 
-    Serial.println("Start LOOP");
 }
 
 void loop() 
 {
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) 
+  if (Serial.available() > 0) 
+  {
+    int data = Serial.parseInt();
+    if(data == 100)
+    {
+      isConfig = true;
+    }
+
+    if(data == 200)
+    {
+      isConfig = false;
+    }
+
+    if(currentButton > 0 && millis() - confPressedTime > 5000)
+    {
+      currentButton = 0;
+    }
+    
+    if(data > 0 && data < 15)
+    {
+      confPressedTime = millis();
+      currentButton = data;
+    }    
+  }
+  
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK && !isConfig) 
   {    
     bool isHandled = false;
     if(canMsg.can_id == 0x21F && canMsg.can_dlc == 3)
     {
       isHandled = true;
-      if(currentButton < 13)
+      if(currentButton & 0b111 > 0)
       {
-        currentButton = 0;
+        currentButton = (currentButton >> 3) << 3;
       }
       
-      if(canMsg.data[1] > prevScroll || prevScroll - canMsg.data[1] > 200)
+      if(canMsg.data[1] > prevScroll || prevScroll - canMsg.data[1] > 200) // scroll up
       {
-        currentButton = 9;
+        currentButton = 5;
       }  
 
-      if(canMsg.data[1] < prevScroll || prevScroll - canMsg.data[1] < -200)
+      if(canMsg.data[1] < prevScroll || prevScroll - canMsg.data[1] < -200) // scroll down
       {
-        currentButton = 10;
+        currentButton = 6;
       }  
 
       prevScroll = canMsg.data[1];
-      if(canMsg.data[0] & 0b00000001)
+      if(canMsg.data[0]  == 4) // volume down
       {
         currentButton = 1;      
       }
 
-      if(canMsg.data[0] & 0b00000010)
+      if(canMsg.data[0] == 8) // volume up
       {
         currentButton = 2;      
       }
 
-      if(canMsg.data[0] & 0b00000100)
+      if(canMsg.data[0] == 12) // mute
       {
         currentButton = 3;      
       }
 
-      if(canMsg.data[0] & 0b00001000)
+      if(canMsg.data[0] == 128) // forward
       {
         currentButton = 4;      
       }
-
-      if(canMsg.data[0] & 0b00001100)
-      {
-        currentButton = 11;      
-      }
-
-      if(canMsg.data[0] & 0b00010000)
-      {
-        currentButton = 5;      
-      }
-
-      if(canMsg.data[0] & 0b00100000)
-      {
-        currentButton = 6;      
-      }
-
-      if(canMsg.data[0] & 0b01000000)
-      {
-        currentButton = 7;      
-      }
-
-      if(canMsg.data[0] & 0b10000000)
-      {
-        currentButton = 8;      
-      }
-
-      if(canMsg.data[0] & 0b11000000)
-      {
-        currentButton = 12;      
-      }
-           Serial.print("Buttons 1 ");
-      
+            
       log(canMsg, true);
+      Serial.print("Btn "); Serial.println(currentButton);  
+      Serial.print("Scr1 "); Serial.println(prevScroll); 
     }
     
-    if(canMsg.can_id == 0x0A2 && canMsg.can_dlc == 6)
+    if(canMsg.can_id == 0xA2 && canMsg.can_dlc == 5)
     {
       isHandled = true;
-      if(currentButton > 12)
+      if(currentButton > 7)
       {
-        currentButton = 0;
+        currentButton = currentButton & 0b111;
       }
       
       if(canMsg.data[0] > prevScroll2 || prevScroll2 - canMsg.data[0] > 200)
       {
-        currentButton = 13;
+        currentButton = 8;
       }  
 
       if(canMsg.data[0] < prevScroll2 || prevScroll2 - canMsg.data[0] < -200)
       {
-        currentButton = 14;
+        currentButton = 9;
       }  
 
       prevScroll2 = canMsg.data[0];       
 
-      if(canMsg.data[3] > prevScroll3 || prevScroll3 - canMsg.data[3] > 200)
-      {
-        currentButton = 15;
-      }  
+      //if(canMsg.data[3] > prevScroll3 || prevScroll3 - canMsg.data[3] > 200)
+      //{
+      //  currentButton = 15;
+      //}  
 
-      if(canMsg.data[3] < prevScroll3 || prevScroll3 - canMsg.data[3] < -200)
-      {
-        currentButton = 16;
-      }  
+      //if(canMsg.data[3] < prevScroll3 || prevScroll3 - canMsg.data[3] < -200)
+      //{
+      //  currentButton = 16;
+      //}  
 
-      prevScroll3 = canMsg.data[3];
-            Serial.print("Buttons 2 ");
+      if(canMsg.data[1]  == 4) // 
+      {
+        currentButton = 10;      
+      }
+
+      if(canMsg.data[0] == 8) //
+      {
+        currentButton = 11;      
+      }
+
+      if(canMsg.data[1]  == 16) // 
+      {
+        currentButton = 12;      
+      }
+
+      if(canMsg.data[0] == 32) //
+      {
+        currentButton = 13;      
+      }
+
+      //prevScroll3 = canMsg.data[3];
       log(canMsg, true);
+      Serial.print("Btn "); Serial.println(currentButton);  
+      Serial.print("Scr3 "); Serial.println(prevScroll3); 
+      Serial.print("Scr2 "); Serial.println(prevScroll2); 
     }    
 
-    // задний ход
+    // задний ход все верно
     if(canMsg.can_id == 0x0F6 && canMsg.can_dlc == 8)
     {
-      isHandled = true;      
+        isHandled = true;      
         if (canMsg.data[7] & 0b10000000) 
         {
           if(!prevReverse)
@@ -187,21 +203,22 @@ void loop()
             digitalWrite(REVERSEPIN, LOW);          
           }
         }
-
-              Serial.print("Reverse ");
+                
       log(canMsg, true);    
+      Serial.print("Reverse "); Serial.println(prevReverse);
     }    
 
     // время
-    if(canMsg.can_id == 0x276 && canMsg.can_dlc > 4)
+    if(canMsg.can_id == 0x3f6 && canMsg.can_dlc == 7)
     {  
-            isHandled = true;    
-            Serial.print("Time ");
+      isHandled = true;  
       log(canMsg, true);
-
-      float coef = (180 - ((canMsg.data[1] & 0b00001111)-1)*30+(canMsg.data[2] & 0b00011111))/180.0; 
+      float day = ((canMsg.data[3] & 0b00001111) << 4) + (canMsg.data[4] >> 4);
+      float month = canMsg.data[4] & 0b00001111;
+      float year = canMsg.data[5] + 2000;
+      float coef = (180 - (month-1)*30+day)/180.0; 
       coef = coef < 0 ? -coef : coef;
-      float currentTime = canMsg.data[3] + canMsg.data[4]/60.0;
+      float currentTime = ((canMsg.data[0] << 12) + (canMsg.data[1] << 4) + (canMsg.data[2] >> 4))/3600.0;
       if(currentTime < 3.5 + 6.0/coef || currentTime > 21.5 - 5.5/coef)
       {
         if(!prevIlluminate)
@@ -218,11 +235,8 @@ void loop()
           digitalWrite(ILLUMINATEPIN, LOW);
         }        
       }
-    }    
 
-    if(!isHandled)
-    {
-      log(canMsg, false);
+       Serial.print("Illuminate "); Serial.print(prevIlluminate); Serial.print("Date "); Serial.print(day); Serial.print("."); Serial.print(month); Serial.print("."); Serial.print(year); Serial.print(" Time "); Serial.println(currentTime);  
     }
  }
   
@@ -236,7 +250,6 @@ void loop()
 
 void pressButton(int value)
 {
-   Serial.println("button pressed " + value);
   if(value == 0)
   {
     setResistance(0);
@@ -248,9 +261,13 @@ void pressButton(int value)
 }
 
 void log(struct can_frame can, bool isParsed)
-{    
+{
+    if(isParsed)
+    {
+      Serial.print("Defined ");
+    }
+    
     Serial.print(can.can_id, HEX); // print ID
-    Serial.print(" ");
     Serial.print(can.can_dlc, HEX); // print DLC
     Serial.print(" ");
     
