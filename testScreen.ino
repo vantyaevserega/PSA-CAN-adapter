48d9dc57f442aab78493088dcf8c221e823389d2
vantyaevserega <33132682+vantyaevserega@users.noreply.github.com>
2022-03-05 15:28:45 +0500

    Пройдена отладка через консоль, добавляю кнопки и джойстик

--- testScreen.ino
+++ testScreen.ino
@@ -1,20 +1,54 @@
 #include <mcp2515.h>
 #include <SPI.h>
 
+#define PIN_VRX  A0
+#define PIN_VRY  A1
+#define PIN_ok   3
+#define PIN_esc  4
+#define PIN_menu 5
+#define PIN_mode 6
+#define PIN_dark 7
+
+bool lastOk   = LOW;
+bool lastEsc  = LOW;
+bool lastMode = LOW;
+bool lastMenu = LOW;
+bool lastDark = LOW;
+int nullValue = 512;
+
 struct can_frame canMsg;
 MCP2515 mcp2515(10);
+unsigned long pr;
 unsigned long prevChangeButtonState;
 unsigned long prevInfoState;
 unsigned long prevF6;
 unsigned long prev36;
+bool emulate = false;
 struct can_frame data1;
 struct can_frame data2;
 struct can_frame data3;
 struct can_frame data_info;
 int currentButton = 0;
 int infos;
+
+bool debounce(int pinValue, int lastButton) 
+{
+  bool current = digitalRead(pinValue);
+  if (current != lastButton) 
+  {                  
+    delay(5);                              
+    current = digitalRead(pinValue);      
+  }
+  
+  return current;
+}
+  
 void setup() 
 {
+  pinMode(PIN_esc, INPUT);
+  pinMode(PIN_menu, INPUT);
+  pinMode(PIN_mode, INPUT);
+  pinMode(PIN_dark, INPUT);
   data1.can_id  = 0x036;
   data1.can_dlc = 8;
   data1.data[0] = 0x0E;
@@ -49,13 +83,13 @@ void setup()
   data_info.can_id  = 0x1A1;
   data_info.can_dlc = 8;
   data_info.data[0] = 0x80;
-  data_info.data[1] = 0;
-  data_info.data[2] = 0x46;
-  data_info.data[3] = 0x80;
-  data_info.data[4] = 0;
-  data_info.data[5] = 0;
-  data_info.data[6] = 0;
-  data_info.data[7] = 0;
+  data_info.data[1] = 0x01;
+  data_info.data[2] = 0x80;
+  data_info.data[3] = 0x00;
+  data_info.data[4] = 0x00;
+  data_info.data[5] = 0x00;
+  data_info.data[6] = 0x00;
+  data_info.data[7] = 0x00;
   
   Serial.begin(115200);
   Serial.println("Start work");
@@ -67,19 +101,68 @@ void setup()
 
 void loop() 
 {
+  int x = analogRead(PIN_VRX);
+  int y = analogRead(PIN_VRY);
+  if(x < nullValue - 100)
+  { currentButton = 4;}
+  else if( x > nullValue + 100)
+  { currentButton = 6; }
+  else if( y < nullValue - 100)
+  { currentButton = 2; }
+  else if( y > nullValue + 100)
+  { currentButton = 8; }
+  else 
+  { 
+    int currentOk = debounce(PIN_ok, lastOk); 
+    if (lastOk == LOW && currentOk == HIGH)
+    { currentButton = 5; }
+    lastOk = currentOk; 
+
+    int currentEsc = debounce(PIN_esc, lastEsc); 
+    if (lastEsc == LOW && currentEsc == HIGH)
+    { currentButton = 7; }
+    lastEsc = currentEsc; 
+
+    int currentMode = debounce(PIN_mode, lastMode); 
+    if (lastMode == LOW && currentMode == HIGH)
+    { currentButton = 7; }
+    lastMode = currentMode; 
+
+    int currentDark = debounce(PIN_dark, lastDark); 
+    if (lastDark == LOW && currentDark == HIGH)
+    { currentButton = 1; }
+    lastDark = currentDark; 
+
+    int currentMenu = debounce(PIN_menu, lastMenu); 
+    if (lastMenu == LOW && currentMenu == HIGH)
+    { currentButton = 9; }
+    lastMenu = currentMenu; 
+  }
+  
   // обработка серийного порта, для конфигурации
   if (Serial.available() > 0) 
   {
     char readed = (char)Serial.read();
-    if(readed >= 48 && readed < 57)
+    Serial.println((int)readed);
+    if(readed > 48 && readed <= 57)
     {
       currentButton = readed - '0';
     }
     else
     {
-      if(readed == 'A')
+      if(readed == 65)
       {
         infos = 0;
+      }
+      else
+      if(readed == 'B')
+      { 
+        emulate = true;
+      }
+      else
+      if(readed == 'C')
+      { 
+        emulate = false;
       }
     }    
   }
@@ -89,26 +172,41 @@ void loop()
     infos = -1;
   }
 
-  if(infos > -1 && infos < 256 && millis() - prevInfoState >= 5000)
+  if(emulate && millis() - pr >= 1000)
   {
+  
+  struct can_frame dat;
+  dat.can_id  = 0x276;
+  dat.can_dlc = 7;
+  dat.data[0] = 0x95;
+  dat.data[1] = 0x03;
+  dat.data[2] = 0x05;
+  dat.data[3] = 0x08;
+  dat.data[4] = 0x10;
+  dat.data[5] = 0x1B;
+  dat.data[6] = 0x10;
+
     data_info.data[1] = infos;
-    ++infos;
+    if(millis()-prevInfoState > 5000){++infos; prevInfoState = millis();}
     mcp2515.sendMessage(&data_info);
+    mcp2515.sendMessage(&dat);
+ 
+    pr = millis();
   }
   
-  if(millis() - prev36 >= 100 )
+  if(emulate && millis() - prev36 >= 100 )
   {
     mcp2515.sendMessage(&data1);
     prev36 = millis();
   }
 
-  if(millis() - prevF6 >= 500)
+  if(emulate && millis() - prevF6 >= 500)
   {
     mcp2515.sendMessage(&data2);
     prevF6 = millis();
   }
   
-  if(millis() - prevChangeButtonState > 50 && currentButton > 0)
+  if(millis() - prevChangeButtonState >= 99)
   {
       switch(currentButton)
       {          
@@ -140,7 +238,8 @@ void loop()
         data3.data[2] = 4;
           break;
       }
-      
+if(currentButton > 0)
+      Serial.println(currentButton);
       mcp2515.sendMessage(&data3);
       currentButton = 0;
       data3.data[0] = 0;
@@ -149,7 +248,7 @@ void loop()
       data3.data[3] = 0;
       data3.data[4] = 0;
       data3.data[5] = 0;
-      mcp2515.sendMessage(&data3);
+      //mcp2515.sendMessage(&data3);
       prevChangeButtonState = millis();
   }
 
